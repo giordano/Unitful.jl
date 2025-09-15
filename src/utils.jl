d14ad5bc72a53587dc19d0d095c4ee07e85c5585
Santtu Söderholm <santtu.soderholm@protonmail.com>
2025-09-15 11:37:05 +0300

    Prevent infinite recursion by adding a method unit(::Type{Any}) (#807)

--- src/utils.jl
+++ src/utils.jl
@@ -145,6 +145,11 @@ true
 @inline unit(x::Type{Missing}) = missing
 @inline unit(x::Missing) = missing
 
+# Prevent infinite recursion, in case unit(Any) is called.
+@inline unit(x::Type{Any}) = throw(ArgumentError(
+    "unit(Any) was called, which has no meaningful result. This may be due to calling unit(eltype(…)), since eltype has a generic fallback method that returns Any."
+))
+
 """
     absoluteunit(::Units)
     absoluteunit(::Quantity)
